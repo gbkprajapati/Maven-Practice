0d681919090236efd5296f79014c5f85f36a21a6
Gaurav Prajapati <gbkprajapati@gmail.com>
2019-03-07 14:20:46 -0500

    Maven Practice
    Install to basic test case

---
+++ MavenProject/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void Login() {
+		System.out.println("Welcome to Appium Login Page :");
+	}
+	@Test
+	public void Logout() {
+		System.out.println("Welcome to Appium Logout Page :");
+	}
+	
+}
---
+++ MavenProject/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void Login() {
+		System.out.println("Welcome to Selenium Login Page :");
+	}
+	@Test
+	public void Logout() {
+		System.out.println("Welcome to Selenium Logout Page :");
+	}
+	
+}
